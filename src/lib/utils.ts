78ebbc5054056b3e37b373751f0cb78da29d9f91
Gabriel Carniel <gpcarniel@outlook.com>
2023-10-29 20:13:11 -0300

    formatar moeda

---
+++ src/lib/utils.ts
@@ -0,0 +1,11 @@
+export const formatPrice = (price: number | null) => {
+  if (!price) {
+    return 'R$ 0,00'
+  }
+
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 2,
+  }).format(price)
+}
