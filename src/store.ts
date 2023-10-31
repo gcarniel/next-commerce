5e4f533ca4312f4285d10eaaf57a92a1185efe95
Gabriel Carniel <gpcarniel@outlook.com>
2023-10-30 22:02:58 -0300

    refatorando store

--- src/store.ts
+++ src/store.ts
@@ -4,23 +4,41 @@ import { persist } from 'zustand/middleware'
 
 type CartState = {
   cart: ProductType[]
-  addToCart: (product: ProductType) => void
-  removeFromCart: (productId: string) => void
+  addProduct: (product: ProductType) => void
+  removeProduct: (productId: string) => void
   isOpen: boolean
   toggleCart: () => void
+  clearCart: () => void
 }
 export const useCartStore = create<CartState>()(
   persist(
     (set) => ({
       cart: [],
-      addToCart: (product) =>
-        set((state) => ({ cart: [...state.cart, product] })),
-      removeFromCart: (productId) =>
+      addProduct: (product) =>
+        set((state) => {
+          const existingProduct = state.cart.find((p) => p.id === product.id)
+
+          if (existingProduct) {
+            return {
+              cart: state.cart.map((p) => {
+                if (p.id === product.id) {
+                  return { ...p, quantity: p.quantity ? p.quantity + 1 : 1 }
+                }
+
+                return p
+              }),
+            }
+          }
+
+          return { cart: [...state.cart, { ...product, quantity: 1 }] }
+        }),
+      removeProduct: (productId) =>
         set((state) => ({
           cart: state.cart.filter((p) => p.id !== productId),
         })),
       isOpen: false,
       toggleCart: () => set((state) => ({ isOpen: !state.isOpen })),
+      clearCart: () => set({ cart: [] }),
     }),
     { name: 'cart-storage' },
   ),
