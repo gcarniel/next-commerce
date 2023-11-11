a590c80940306715d4b8751d26db99efbdfaa460
Gabriel Carniel <gpcarniel@outlook.com>
2023-11-11 11:58:34 -0300

    corrigindo exibicao de itens no carrinho

--- src/app/components/Cart.tsx
+++ src/app/components/Cart.tsx
@@ -7,6 +7,11 @@ import { CartDrawer } from './CartDrawer'
 
 export function Cart() {
   const { toggleCart, cart, isOpen } = useCartStore()
+
+  const totalItems = cart.reduce(
+    (acc, curr) => (curr.quantity ? acc + curr.quantity : acc),
+    0,
+  )
   return (
     <>
       <div
@@ -14,9 +19,11 @@ export function Cart() {
         className="flex items-center cursor-pointer relative"
       >
         <ShoppingCart size={26} />
-        <span className="bg-teal-500 text-sm font-bold rounded-full h-5 w-5 text-center text-gray-700 absolute -top-1 -right-2 animate-bounce">
-          {cart.length}
-        </span>
+        {totalItems > 0 && (
+          <span className="bg-teal-500 text-sm font-bold rounded-full h-5 w-5 text-center text-gray-700 absolute -top-1 -right-2 animate-bounce">
+            {totalItems}
+          </span>
+        )}
       </div>
 
       {isOpen && <CartDrawer />}
